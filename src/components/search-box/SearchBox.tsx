367198a772c66549c3044e42e6f296c8b6d2f878
Panhara28 <titpanhara@gmail.com>
2021-06-16 21:16:21 +0700

    fix

--- src/components/search-box/SearchBox.tsx
+++ src/components/search-box/SearchBox.tsx
@@ -1,10 +1,10 @@
+import { useLazyQuery } from "@apollo/client";
 import Card from "@component/Card";
 import { Span } from "@component/Typography";
-import { ProductContext, ProductProvider } from "@context/app/ProductContext";
+import { SEARCH_PRODUCTS } from "lib/graph";
 import { debounce } from "lodash";
 import Link from "next/link";
 import React, { useCallback, useEffect, useState } from "react";
-import { useContext } from "react";
 import Box from "../Box";
 import Icon from "../icon/Icon";
 import MenuItem from "../MenuItem";
@@ -15,8 +15,38 @@ export interface SearchBoxProps {}
 
 const SearchBox: React.FC<SearchBoxProps> = () => {
   const [resultList, setResultList] = useState([]);
-  const { products, setSearch } = useContext(ProductContext);
-  const [searching, setOnSearch] = useState("");
+  const [searchFilter, setSearchFilter] = useState("");
+
+  const [executeSearch, { data }] = useLazyQuery(SEARCH_PRODUCTS);
+
+  const search = debounce((e) => {
+    const value = e.target?.value;
+    if (!value) setSearchFilter("");
+    else setSearchFilter(e.target.value);
+  });
+
+  const hanldeSearch = useCallback((event) => {
+    event.persist();
+    search(event);
+    console.log(searchFilter);
+
+    executeSearch({
+      variables: {
+        search: searchFilter,
+        limit: 20,
+        offset: 1,
+      },
+    });
+  }, []);
+
+  // const hanldeSearch = (e) => {
+  //   setSearchFilter(e.target.value);
+  // };
+
+  const handleDocumentClick = () => {
+    setResultList([]);
+  };
+
   useEffect(() => {
     window.addEventListener("click", handleDocumentClick);
     return () => {
@@ -24,60 +54,39 @@ const SearchBox: React.FC<SearchBoxProps> = () => {
     };
   }, []);
 
-  const search = debounce((e) => {
-    const value = e.target?.value;
-    if (!value) {
-      setResultList([]);
-    } else {
-      setSearch(value);
-      setResultList(products);
-    }
-  }, 200);
-
-  const hanldeSearch = useCallback((event) => {
-    event.persist();
-    search(event);
-  }, []);
-
-  const handleDocumentClick = () => {
-    setResultList([]);
-  };
-
   return (
-    <ProductProvider search={searching}>
-      <Box position="relative" flex="1 1 0" maxWidth="670px" mx="auto">
-        <StyledSearchBox>
-          <Icon className="search-icon" size="18px">
-            search
-          </Icon>
-          <TextField
-            className="search-field"
-            placeholder="Search and hit enter..."
-            fullwidth
-            onChange={hanldeSearch}
-          />
-        </StyledSearchBox>
-        {/* 
-        {!!resultList.length && (
-          <Card
-            position="absolute"
-            top="100%"
-            py="0.5rem"
-            width="100%"
-            boxShadow="large"
-            zIndex={99}
-          >
-            {resultList.map((item) => (
-              <Link href={`/product/search/${item}`} key={item.id}>
-                <MenuItem key={item.id}>
-                  <Span fontSize="14px">{item.product_name}</Span>
-                </MenuItem>
-              </Link>
-            ))}
-          </Card>
-        )} */}
-      </Box>
-    </ProductProvider>
+    <Box position="relative" flex="1 1 0" maxWidth="670px" mx="auto">
+      <StyledSearchBox>
+        <Icon className="search-icon" size="18px">
+          search
+        </Icon>
+        <TextField
+          className="search-field"
+          placeholder="Search and hit enter..."
+          fullwidth
+          onChange={hanldeSearch}
+        />
+      </StyledSearchBox>
+
+      {!!resultList.length && (
+        <Card
+          position="absolute"
+          top="100%"
+          py="0.5rem"
+          width="100%"
+          boxShadow="large"
+          zIndex={99}
+        >
+          {resultList.map((item) => (
+            <Link href={`/product/search/${item}`} key={item}>
+              <MenuItem key={item}>
+                <Span fontSize="14px">{item}</Span>
+              </MenuItem>
+            </Link>
+          ))}
+        </Card>
+      )}
+    </Box>
   );
 };
 
