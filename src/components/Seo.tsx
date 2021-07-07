4af831ced95f6b68195ba05b58b807b88e87bbe4
panhara <titpanhara@gmail.com>
2021-07-07 10:23:10 +0700

    fix

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -15,21 +15,7 @@ export const SEO: React.FC<SeoProps> = ({
   image,
 }) => (
   <Head>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;900&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <link rel="manifest" href="/site.webmanifest" />
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-    <meta name="apple-mobile-web-app-title" content="bonik" />
-    <meta name="application-name" content="bonik" />
-    <meta name="msapplication-TileColor" content="#da532c" />
-    <meta name="theme-color" content="#ffffff" />
-
-    <title>{title ? title + " | Loktomninh.com" : "Loktomninh.com"}</title>
+    <title>{title ? title + " | loktomninh.com" : "loktomninh.com"}</title>
     {image ? (
       <meta property="og:image" content={`${image}`} />
     ) : (
@@ -81,5 +67,21 @@ export const SEO: React.FC<SeoProps> = ({
         href={`${canonical ? canonical : "loktomninh.com"}`}
       />
     )}
+
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;900&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="apple-mobile-web-app-title" content="bonik" />
+    <meta name="application-name" content="bonik" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
+
+
   </Head>
 );
